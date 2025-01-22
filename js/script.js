b9b1617da1b0c6d9c1ba6b0b309c8f0f04e8f015
willsouzaa <pauloamancio79@gmail.com>
2025-01-21 22:21:57 -0300

    iniciando projeto site belle vue

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for navbar links
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+});
